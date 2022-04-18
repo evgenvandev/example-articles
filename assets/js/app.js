aa7c73d4fc2eccfdfd4f12a5a6537bfbc53dc689
evgenvandev <developjev@gmail.com>
2022-04-18 08:58:06 +0500

    First_commit

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const headings = document.querySelectorAll('#content .table-nav-item');
+	if (headings && headings.length) {
+		let tableOfContentInner = '';
+		
+		headings.forEach((heading, i) => {
+			// generate an 'li' element that includes a link to the appropriate section
+			tableOfContentInner += `<li><a href="#section_${i}">${heading.textContent}</a></li>`;
+			const originalHeadingContent = heading.innerHTML;
+			const anchor = `<a class="offset-anchor" id="section_${i}"></a>`;
+			// add the anchor to the <h3> tag
+			heading.innerHTML = anchor + originalHeadingContent;
+		});
+		
+		const tableOfContent = `<ol>${tableOfContentInner}</ol>`;
+		// add the generated table of contents to the dive
+		document.querySelector('#table-of-content').innerHTML += tableOfContent;
+		
+		// automatically go to the correct section on load
+		if (location.hash) {
+			const target = location.hash;
+			const offsetY = document.querySelector(target).offsetTop;
+			window.scrollTo(0, offsetY);
+		}
+	}
+});

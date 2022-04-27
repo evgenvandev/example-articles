5d6cfac1711d6c44fe09456ade016e8972b47f56
evgenvandev <developjev@gmail.com>
2022-04-27 17:01:04 +0500

    create adding elements to the beginning javascript

--- Articles_on_Java/Systema_upravleniay_spiskom_kontaktov/assets/source_files/ContactTest.bat
+++ Articles_on_Java/Systema_upravleniay_spiskom_kontaktov/assets/source_files/ContactTest.bat
@@ -1,4 +1,4 @@
-javac -encoding utf-8 -sourcepath ./src -d bin src/edu/javacourse/contact/test/ContactTest.java
+"c:\Program Files\Java\jdk1.8.0_51\bin\javac" -encoding utf-8 -sourcepath ./src -d bin src/edu/javacourse/contact/test/ContactTest.java
 PAUSE
-java -classpath ./bin edu.javacourse.contact.test.ContactTest
+"c:\Program Files\Java\jdk1.8.0_51\bin\java" -classpath ./bin edu.javacourse.contact.test.ContactTest
 PAUSE
